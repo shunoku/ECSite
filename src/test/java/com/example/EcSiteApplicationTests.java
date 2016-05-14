7285ce4cd7758771fb56d109c239863294a5d640
okugawa <okugawa@192.168.0.4>
2016-05-15 04:19:41 +0900

    test

---
+++ src/test/java/com/example/EcSiteApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EcSiteApplication.class)
+@WebAppConfiguration
+public class EcSiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
